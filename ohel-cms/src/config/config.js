e3e09c50c36ecdea957a2c7ee2862ce9413cceb4
S1moT3ch <s.cantore2@studenti.poliba.it>
2026-07-19 18:35:29 +0200

    19-07-2026-18.35

--- ohel-cms/src/config/config.js
+++ ohel-cms/src/config/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    SHEET_ID: "1b78T49TNW1SshXLYpFJuA6lI9ssSIRpq8vKTI1EhyR8",
+    SHEET_ID: "17184vz2D4PS_L-E99gCqdvtIeGqE0_5ar3UuyJiguK4",
     API_KEY: "AIzaSyDb0HRCmZP1BesThMKKSCQZ5SUZogR_uw0",
     RANGE: "Risposte del modulo 1",
 };
